2bc5b690e9b1d30ec57cb92b0b3b9b1dd144d1fa
Malnati <ricardomalnati@gmail.com>
2024-09-16 07:39:54 -0300

    Refactor GetOrganicaService and GetPoluicaoDataService to update API endpoint paths and add constructor for base URL support

--- src/services/GetOrganicaService.ts
+++ src/services/GetOrganicaService.ts
@@ -4,12 +4,17 @@ import Service from './Service';
 import { IPercentualAreaChart, IStackedAreaChart } from '../types';
 
 class GetOrganicaService extends Service {
+
+    constructor() {
+        super(process.env.REACT_APP_API_BASE_URL, '/organicas');
+    }
+
     public async getStackedData(): Promise<IStackedAreaChart[]> {
-        return this.get<IStackedAreaChart[]>('/organicas/stacked');
+        return this.get<IStackedAreaChart[]>('/stacked');
     }
 
     public async getOrganicasAsPercentual(): Promise<IPercentualAreaChart[]> {
-        return this.get<IPercentualAreaChart[]>('/organicas/percentual');
+        return this.get<IPercentualAreaChart[]>('/percentual');
     }
 }
 
--- src/services/GetPoluicaoDataService.ts
+++ src/services/GetPoluicaoDataService.ts
@@ -4,12 +4,17 @@ import { IStackedAreaChart } from '../types';
 import Service from './Service';
 
 class GetPoluicaoDataService extends Service {
+
+    constructor() {
+        super(process.env.REACT_APP_API_BASE_URL, '/poluicao');
+    }
+
     public async getStackedData(): Promise<IStackedAreaChart[]> {
-        return this.get<IStackedAreaChart[]>('/poluicao/stacked');
+        return this.get<IStackedAreaChart[]>('/stacked');
     }
 
     public async getPercentualData(): Promise<IStackedAreaChart[]> {
-        return this.get<IStackedAreaChart[]>('/poluicao/percentual');
+        return this.get<IStackedAreaChart[]>('/percentual');
     }
 }
 

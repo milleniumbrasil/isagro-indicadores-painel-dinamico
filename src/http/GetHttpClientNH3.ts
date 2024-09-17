b2ae823a272bff6adc7e2db5a57d7598aef10865
Malnati <ricardomalnati@gmail.com>
2024-09-17 19:59:55 -0300

    Refactor GetHttpClientNH3 to update API endpoint path for getting stacked NH3 data

--- src/http/GetHttpClientNH3.ts
+++ src/http/GetHttpClientNH3.ts
@@ -6,7 +6,7 @@ import BaseGetHttpClient from './BaseGetHttpClient';
 class GetHttpClientNH3 extends BaseGetHttpClient {
 
     constructor() {
-        super(process.env.REACT_APP_API_BASE_URL, '/nh3');
+        super(process.env.REACT_APP_API_BASE_URL, '/nh3s');
     }
 
     public async getStackedData(): Promise<IStackedAreaChart[]> {

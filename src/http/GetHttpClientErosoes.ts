cdf29f43763f79be465f12362e5d54e1449065d7
Malnati <ricardomalnati@gmail.com>
2024-09-18 14:34:47 -0300

    Refactor input field styling in PaperNH3, PaperNPK, and PaperOrganicas components

--- src/http/GetHttpClientErosoes.ts
+++ src/http/GetHttpClientErosoes.ts
@@ -1,20 +1,20 @@
 // src/services/GetHttpClientErosao.ts
 
-import { IStackedAreaChart } from '../types';
+import { IPercentualAreaChart, IStackedAreaChart } from '../types';
 import BaseGetHttpClient from './BaseGetHttpClient';
 
 class GetHttpClientErosoes extends BaseGetHttpClient {
 
     constructor() {
-        super(process.env.REACT_APP_API_BASE_URL, '/erosao');
+        super(process.env.REACT_APP_API_BASE_URL, '/erosoes');
     }
 
     public async getStackedData(): Promise<IStackedAreaChart[]> {
         return this.get<IStackedAreaChart[]>('/stacked');
     }
 
-    public async getPercentualData(): Promise<IStackedAreaChart[]> {
-        return this.get<IStackedAreaChart[]>('/percentuals');
+    public async getPercentualData(): Promise<IPercentualAreaChart[]> {
+        return this.get<IPercentualAreaChart[]>('/percentuals');
     }
 
     // Adicionando método com parâmetro de label
---
+++ src/pages/ErosoesPage.tsx
@@ -0,0 +1,73 @@
+// src/pages/ErosoesPage.tsx
+
+import './ErosoesPage.css';
+
+import { FC, useEffect, useState, Suspense } from 'react';
+import { useErosoesContext } from '../components/ErosoesContext';
+import { IPercentualAreaChart, IStackedAreaChart } from '../types';
+import PaperErosoes from '../components/PaperErosoes';
+
+export function Loading() {
+    return (
+        <p>
+            <i>Loading...</i>
+        </p>
+    );
+}
+
+const ErosoesPage: FC = () => {
+    // dados do servidor armazenados no contexto
+    const { contextStates: contextStates } = useErosoesContext();
+    const { contextCountries: contextCountries } = useErosoesContext();
+    const { contextCities } = useErosoesContext();
+    const { contextStackedData: contextErosoesStackedData } = useErosoesContext();
+    const { contextPercentualData: contextErosoesPercentual } = useErosoesContext();
+
+    const [internalErosoesStacked, setInternalErosoesStacked] = useState<IStackedAreaChart[]>([]);
+    const [internalErosoesPercentual, setInternalErosoesPercentual] = useState<IPercentualAreaChart[]>([]);
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (contextErosoesPercentual && contextErosoesPercentual.length > 0) {
+                    setInternalErosoesPercentual(contextErosoesPercentual);
+                    console.log(`[ErosoesPage] internalErosoesPercentual loaded from context: ${contextErosoesPercentual.length}`);
+                } else {
+                    console.warn('[ErosoesPage] contextErosoesPercentual is required');
+                }
+                if (contextErosoesStackedData && contextErosoesStackedData.length > 0) {
+                    setInternalErosoesStacked(contextErosoesStackedData);
+                    console.log(`[ErosoesPage] internalErosoesStacked loaded from context: ${internalErosoesStacked.length}`);
+                } else {
+                    console.warn('[ErosoesPage] contextErosoesStackedData is required');
+                }
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [contextErosoesPercentual, contextErosoesStackedData]);
+
+    return (
+        <Suspense fallback={<Loading />}>
+            {internalErosoesPercentual.length > 0 ? (
+                <PaperErosoes
+                    stackedData={internalErosoesStacked}
+                    percentualData={internalErosoesPercentual}
+                    countries={contextCountries}
+                    states={contextStates}
+                    cities={contextCities}
+                />
+            ) : (
+                <Loading />
+            )}
+        </Suspense>
+    );
+};
+
+export default ErosoesPage;

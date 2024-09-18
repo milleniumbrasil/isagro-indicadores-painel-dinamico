d4af156ae7f987210e710c09c0067690f78283cf
Malnati <ricardomalnati@gmail.com>
2024-09-18 08:11:39 -0300

    Refactor App component to add NPKPage and NPKProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 // src/App.tsx
 
 import React from 'react';
+
 import OrganicasPage from './pages/OrganicasPage';
 import { OrganicasProvider } from './components/OrganicasProvider';
 
+import NPKPage from './pages/NPKPage';
+import { NPKProvider } from './components/NPKProvider';
+
 const App: React.FC = () => {
     return (
+        <>
         <OrganicasProvider>
             <OrganicasPage />
         </OrganicasProvider>
+        {/* <NPKProvider>
+            <NPKPage />
+        </NPKProvider> */}
+        </>
     );
 };
 

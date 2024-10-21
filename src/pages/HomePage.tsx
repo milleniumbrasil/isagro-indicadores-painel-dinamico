9c7a003dbf678caecd1f4ce3d97646cd69f3cec0
Malnati <ricardomalnati@gmail.com>
2024-10-21 09:41:24 -0300

    Refactor HomePage component to update layout and typography

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -42,83 +42,97 @@ const Media: FC<MediaProps> = (props: MediaProps) => {
     };
 
     return (
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-            {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
-                <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
-                    <Box key={index} sx={{ width: 210, marginRight: 0.5, my: 5 }}>
-                        {item ? (
-                        <img
-                            style={{ width: 210, height: 118 }}
-                            alt={item.title}
-                            src={item.src}
-                        />
-                        ) : (
-                        <Skeleton variant="rectangular" width={210} height={118} />
-                        )}
-                        {item ? (
-                        <Box sx={{ pr: 2 }}>
-                            <Typography gutterBottom variant="body2">
-                            {item.title}
-                            </Typography>
-                            <Typography
-                            variant="caption"
-                            sx={{ display: 'block', color: 'text.secondary' }}
-                            >
-                            {item.sub}
-                            </Typography>
-                            <Typography
-                            variant="caption"
-                            sx={{
-                                display: 'block',
-                                color: 'text.secondary',
-                                maxHeight: expanded[index] ? 'none' : `${TEXT_MAX_HEIGHT}px`,
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                                transition: 'max-height 0.3s ease',
-                            }}
-                            >
-                            {item.desc}
-                            </Typography>
-                            <Typography
-                            variant="caption"
-                            sx={{
-                                display: 'block',
-                                color: 'text.secondary',
-                                maxHeight: expanded[index] ? 'none' : `${TEXT_MAX_HEIGHT}px`,
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                                transition: 'max-height 0.3s ease',
-                            }}
-                            >
-                            {item.obs}
-                            </Typography>
-                            <Button size="small">Share</Button>
-                            <Button size="small" onClick={() => handleToggleExpand(index)}>
-                                {expanded[index] ? 'Voltar' : 'Saiba mais'}
-                            </Button>
-                        </Box>
-                        ) : (
-                        <Box sx={{ pt: 0.5 }}>
-                            <Skeleton />
-                            <Skeleton width="60%" />
-                        </Box>
-                        )}
+        <>
+            <Box sx={{ display: 'flex', '& > :not(style)': { m: 1 } }}>
+                <Box sx={{ flexGrow: 1, margin: '30px', padding: '30px' }}>
+                    <img src="/logo-isagro.png" alt="Logo Isagro" style={{ width: '150px', height: 'auto', margin: '15px' }} />
+                    <Typography variant="h2" sx={{ textAlign: 'center', padding: '15px', margin: '15px' }}>
+                        Indicadores Agro-socioambientais do Brasil
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom style={{ height: 'auto', margin: '30px' }}>
+                    Os indicadores agro-sociombientais constituem um conjunto de informações quantitativas capazes de identificar, avaliar e monitorar os impactos ambientais, sociais e econômicos relacionados às atividades agropecuárias.  Métricas e Indicadores subsidiam a tomada de decisão por parte de agentes governamentais e da iniciativa privada, estabelecendo informações de grande valor estratégico e auxiliando nos processos de planejamento agroambiental e de gestão de políticas públicas. Isso permite identificar aspectos sensíveis e pontos fortes, modelar cenários e traçar tendências, contribuindo para a avaliação da sustentabilidade e resiliencia da agropecuária brasileira. Esse ambiente digital disponibiliza indices e indicadores agro-socioambientais calculados após ajustes técnicos realizados pela equipe do Projeto IS_Agro (proposição do Ministério da Agricultura e Pecuária (MAPA), execução da Embrapa - Embrapa Solos, Embrapa Agrobiologia, Embrapa Meio Ambiente e a Assessoria Internacional da Embrapa em parceria com o Serviço Geológico do Brasil (SGB/CPRM).
+                    </Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', margin: '50px' }}>
+                        <img src="/MAPA_EMBRAPA_SGB_CPRM.jpeg" alt="MAPA e da EMBRAPA E SGB/CPRM" style={{ width: '700px', height: 'auto' }} />
                     </Box>
+                </Box>
+            </Box>
+
+            <Container maxWidth="sm">
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
+                        <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
+                            <Box key={index} sx={{ width: 210, marginRight: 0.5, my: 5 }}>
+                                {item ? (
+                                <img
+                                    style={{ width: 210, height: 118 }}
+                                    alt={item.title}
+                                    src={item.src}
+                                />
+                                ) : (
+                                <Skeleton variant="rectangular" width={210} height={118} />
+                                )}
+                                {item ? (
+                                <Box sx={{ pr: 2 }}>
+                                    <Typography gutterBottom variant="body2">
+                                    {item.title}
+                                    </Typography>
+                                    <Typography
+                                    variant="caption"
+                                    sx={{ display: 'block', color: 'text.secondary' }}
+                                    >
+                                    {item.sub}
+                                    </Typography>
+                                    <Typography
+                                    variant="caption"
+                                    sx={{
+                                        display: 'block',
+                                        color: 'text.secondary',
+                                        maxHeight: expanded[index] ? 'none' : `${TEXT_MAX_HEIGHT}px`,
+                                        overflow: 'hidden',
+                                        textOverflow: 'ellipsis',
+                                        transition: 'max-height 0.3s ease',
+                                    }}
+                                    >
+                                    {item.desc}
+                                    </Typography>
+                                    <Typography
+                                    variant="caption"
+                                    sx={{
+                                        display: 'block',
+                                        color: 'text.secondary',
+                                        maxHeight: expanded[index] ? 'none' : `${TEXT_MAX_HEIGHT}px`,
+                                        overflow: 'hidden',
+                                        textOverflow: 'ellipsis',
+                                        transition: 'max-height 0.3s ease',
+                                    }}
+                                    >
+                                    {item.obs}
+                                    </Typography>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small" onClick={() => handleToggleExpand(index)}>
+                                        {expanded[index] ? 'Voltar' : 'Saiba mais'}
+                                    </Button>
+                                </Box>
+                                ) : (
+                                <Box sx={{ pt: 0.5 }}>
+                                    <Skeleton />
+                                    <Skeleton width="60%" />
+                                </Box>
+                                )}
+                            </Box>
+                        </Grid>
+                    ))}
                 </Grid>
-            ))}
-        </Grid>
+            </Container>
+        </>
     );
 };
 
 
 const HomePage: FC = () => {
     return (
-        <Container maxWidth="sm">
-            <Typography variant="h4" gutterBottom>
-                Home Page
-            </Typography>
             <Media loading={false} />
-        </Container>
     );
 }
 
